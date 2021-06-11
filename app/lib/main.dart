50106903d9dc7ea86831e7ac53230d23077def6f
rntcursos <rntcursos@gmail.com>
2021-06-11 20:32:30 -0300

    inicio do estudo

---
+++ app/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Estudo web',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
